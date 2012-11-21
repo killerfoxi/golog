1228fcbc8cb366f72c62a3cf323b14c57f30a342
René Nussbaumer <killerfoxi@gmail.com>
2012-11-21 19:44:38 +0100

    Adding some additional logging

--- test.go
+++ test.go
@@ -4,6 +4,12 @@ import (
   "./golog"
 )
 
+func foo() {
+  golog.Info("Entered foo()")
+  golog.Debug("Something is fishy!")
+  golog.Info("Leaving foo()")
+}
+
 func main() {
   golog.Current.SetSeverity(golog.SeverityDebug)
   golog.Debug("Test debug log")
@@ -11,4 +17,5 @@ func main() {
   golog.Warning("Test warning log")
   golog.Error("Test error log")
   golog.Fatal("Test fatal log")
+  foo()
 }
